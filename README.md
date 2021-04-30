17a0c72cde3070e70233ab8295565d90a73c28d3
Madhuri Chitikela <madhurisravan.chitikela@gmail.com>
2021-04-30 21:32:44 +0530

    adding README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Color_Flipper application
+
+description: to be written
+
+[View Demo here](https://madhuri-chitikela.github.io/color_flipper/)
+
+![Screenshot](docs/Capture.png)
+
+## Tech stack
+
+- HTML
+- CSS
+- Bootstrap
+- Vanilla JavaScript
+
+## Courtesy tutorial
+
+[Build 15 JavaScript Projects - Vanilla JavaScript Course](https://www.youtube.com/watch?v=3PHXvlpOkf4)
--- hex.js
+++ hex.js
@@ -1,14 +1,16 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
 btn.addEventListener("click", function () {
-    let hexcolor = "#"
+    let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        hexcolor += hex[getRandomNumber()]
+        hexColor += hex[getRandomNumber()];
     }
-    document.body.style.backgroundColor = hexcolor
-    document.textContent = hexcolor
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
 })
+
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
